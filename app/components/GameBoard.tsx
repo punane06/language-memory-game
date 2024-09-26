bc8549d0a659d342ef1fb0f0082ea37d70703b72
Kadi Kerner <kadikerner@gmail.com>
2024-09-26 15:34:01 +0300

    git init

---
+++ app/components/GameBoard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card'
+
+const GameBoard = () => {
+    return (
+        <div>
+            <h1>
+                GameBoard
+            </h1>
+            <Card />
+        </div>
+    )
+}
+
+export default GameBoard
