5f17abcb5d9cf4273d577d18ec45c13b2a30b496
kkristinavv <kristina.valk@gmail.com>
2024-10-02 16:27:45 +0300

    header
    Language selector added into header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,20 +3,22 @@ import LanguagePick from "./LanguagePick";
 import Button from './Button';
 import ArrowDownIcon from './icons/ArrowDownIcon';
 
-const Header = () => {
+const Header = ({ onLanguageChange }: { onLanguageChange: (language: string) => void }) => {
   return (
     <header className="header">
- 
-      <div className="header-container">
       <h1 className="header-title">FIND THE PAIRS</h1>
-        <div className='header-actions'>
-        <Button
+
+      <div className='header-actions-container'>
+        <div className='header-language'>
+          <LanguagePick onChange={onLanguageChange} />
+        </div>
+        <div className="header-actions">
+          <Button
             title='00:20' >
           </Button>
           <Button
-            onlyIcon={<ArrowDownIcon width={18} height={18}/>}>
+            onlyIcon={<ArrowDownIcon width={18} height={18} />}>
           </Button>
-         
         </div>
       </div>
     </header>
